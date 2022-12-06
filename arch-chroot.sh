98318ccd2676921bd1b55e0f6905af1d48750f5f
oyinbra <onyinbra@gmail.com>
2022-12-06 20:45:08 +0100

    first commit

---
+++ arch-chroot.sh
@@ -0,0 +1,10 @@
+# Chroot into Arch installed with subvolumes on BTRFS
+lsblk
+mount -o subvol=@ /dev/nvme0n1p3 /mnt
+mount -o subvol=@home /dev/nvme0n1p3 /mnt/home
+mount -o subvol=@cache /dev/nvme0n1p3 /mnt/var/cache
+mount -o subvol=@log /dev/nvme0n1p3 /mnt/var/log
+mount /dev/nvme0n1p1 /mnt/boot/efi
+swapon /dev/nvme0n1p2
+lsblk
+arch-chroot /mnt
