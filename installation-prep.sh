f7c428facdf5c2856d34b4809ced2cd1035463d7
oyinbra <onyinbra@gmail.com>
2022-12-16 21:41:58 +0100

    fixed swap mounting bug

--- installation-prep.sh
+++ installation-prep.sh
@@ -1,8 +1,12 @@
 timedatectl set-ntp true
+loadkeys us
+
+pacman -U rate-mirrors-bin-0.11.1-1-x86_64.pkg.tar.zst
+rate-mirrors --allow-root --protocol https arch | sudo tee /etc/pacman.d/mirrorlist
 
 lsblk
 
-mkfs.btrfs -L archlinux /dev/nvme0n1p2
+mkfs.btrfs -f -L archlinux /dev/nvme0n1p2
 mkfs.fat -F32 /dev/nvme0n1p1
 # mkswap /dev/nvme0n1p2
 
@@ -28,10 +32,11 @@ mount -o noatime,space_cache=v2,ssd,discard=async,subvol=@log /dev/nvme0n1p2 /mn
 mount -o noatime,space_cache=v2,ssd,discard=async,subvol=@tmp /dev/nvme0n1p2 /mnt/var/tmp
 mount -o noatime,space_cache=v2,ssd,discard=async,subvol=@srv /dev/nvme0n1p2 /mnt/srv
 mount -o noatime,space_cache=v2,ssd,discard=async,subvol=@opt /dev/nvme0n1p2 /mnt/opt
-mount -o noatime,space_cache=v2,ssd,subvol=@snapshots /dev/nvme0n1p2 /mnt/swap
+mount -o noatime,space_cache=v2,ssd,subvol=@swap /dev/nvme0n1p2 /mnt/swap
 
 mount /dev/nvme0n1p1 /mnt/boot/efi
-# swapon /dev/nvme0n1p2
 
-lsblk
+cd
+mv arch-easy
+# swapon /dev/nvme0n1p2
 
