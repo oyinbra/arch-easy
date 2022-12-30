fd6ab9c6c54e532640f94b6aac0c14e45c30dee2
oyinbra <onyinbra@gmail.com>
2022-12-30 14:59:28 +0100

    added format and mount for sda partitions

---
+++ format-mount-sda.sh
@@ -0,0 +1,38 @@
+timedatectl set-ntp true
+loadkeys us
+pacman -Sy
+
+lsblk
+
+mkfs.btrfs -f -L ArchLinux /dev/sda3
+mkfs.vfat -n Boot /dev/sda1
+mkswap /dev/sda2
+
+mount /dev/sda3 /mnt
+
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@cache
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@.snapshots
+
+umount -l /mnt
+
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /dev/sda3 /mnt
+
+mkdir -p /mnt/{boot/efi,home,var/cache,var/log,.snapshots}
+
+mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@home /dev/sda3 /mnt/home
+mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@cache /dev/sda3 /mnt/var/cache
+mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@log /dev/sda3 /mnt/var/log
+mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@.snapshots /dev/sda3 /mnt/.snapshots
+
+mount /dev/sda1 /mnt/boot/efi
+
+cd
+cp -r arch-easy /mnt
+
+pacman -S reflector
+reflector --country US --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Sy
+
