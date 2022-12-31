287d28240b988e4ee331bd30c778813364f5ff06
oyinbra <onyinbra@gmail.com>
2022-12-31 06:10:38 +0100

    oh my zsh installation script added with dotfile clone

---
+++ oh-my-zsh-installation.sh
@@ -0,0 +1,16 @@
+echo "#############################################################################"
+echo "#############################################################################"
+echo "##                                                                         ##"
+echo "##                         OH-MY-ZSH INSTALLATION                          ##"
+echo "##                                                                         ##"
+echo "#############################################################################"
+echo "#############################################################################" 
+
+
+sudo pacman -Sy zsh git wget neofetch
+
+cd
+git clone https://github.com/Oyinbra/.dotfiles.git
+
+chsh -s /bin/zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
