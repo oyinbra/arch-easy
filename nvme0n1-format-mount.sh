536f616e058773f16ea6c14fffc6b27b1d751efc
Oyinbra Kelvin <35059691+Oyinbra@users.noreply.github.com>
2023-02-12 12:58:35 +0100

    added image subvolume for libvirt

--- nvme0n1-format-mount.sh
+++ nvme0n1-format-mount.sh
@@ -16,17 +16,20 @@ btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@cache
 btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@images
 btrfs subvolume create /mnt/@.snapshots
+
 
 umount -l /mnt
 
 mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /dev/nvme0n1p3 /mnt
 
-mkdir -p /mnt/{boot/efi,home,var/cache,var/log,.snapshots}
+mkdir -p /mnt/{boot/efi,home,.snapshots,var/{cache,log,lib/libvirt/images}}
 
 mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@home /dev/nvme0n1p3 /mnt/home
 mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@cache /dev/nvme0n1p3 /mnt/var/cache
 mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@log /dev/nvme0n1p3 /mnt/var/log
+mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@log /dev/nvme0n1p3 /mnt/var/lib/libvirt/images
 mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@.snapshots /dev/nvme0n1p3 /mnt/.snapshots
 
 mount /dev/nvme0n1p1 /mnt/boot/efi
