efce47db39517c0f0df08ea952f7f5249a9aa5ba
oyinbra <onyinbra@gmail.com>
2023-04-17 23:14:40 +0100

    💻 removed swap partition

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -1,10 +1,10 @@
 # Chroot into Arch installed with subvolumes on BTRFS
 lsblk
-mount -o subvol=@ /dev/nvme0n1p3 /mnt
-mount -o subvol=@home /dev/nvme0n1p3 /mnt/home
-mount -o subvol=@cache /dev/nvme0n1p3 /mnt/var/cache
-mount -o subvol=@log /dev/nvme0n1p3 /mnt/var/log
-mount -o subvol=@log /dev/nvme0n1p3 /mnt/var/lib/libvirt
+mount -o subvol=@ /dev/nvme0n1p2 /mnt
+mount -o subvol=@home /dev/nvme0n1p2 /mnt/home
+mount -o subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
+mount -o subvol=@log /dev/nvme0n1p2 /mnt/var/log
+mount -o subvol=@log /dev/nvme0n1p2 /mnt/var/lib/libvirt
 mount /dev/nvme0n1p1 /mnt/boot/efi
 swapon /dev/nvme0n1p2
 lsblk
--- nvme0n1-format-mount.sh
+++ nvme0n1-format-mount.sh
@@ -1,16 +1,15 @@
 timedatectl set-ntp true
 loadkeys us
-pacman -Sy reflector archlinux-keyring
-reflector --country US --latest 6 --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Sy
+#pacman -Sy reflector archlinux-keyring
+#reflector --country US --latest 6 --sort rate --save /etc/pacman.d/mirrorlist
+#pacman -Sy
 
 lsblk
 
-mkfs.btrfs -f -L ArchLinux /dev/nvme0n1p3
+mkfs.btrfs -f -L ArchLinux /dev/nvme0n1p2
 mkfs.vfat -n BOOT /dev/nvme0n1p1
-mkswap -L swap /dev/nvme0n1p2
 
-mount /dev/nvme0n1p3 /mnt
+mount /dev/nvme0n1p2 /mnt
 
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
@@ -18,19 +17,19 @@ btrfs subvolume create /mnt/@cache
 btrfs subvolume create /mnt/@log
 btrfs subvolume create /mnt/@images
 btrfs subvolume create /mnt/@.snapshots
+btrfs subvolume create /mnt/@swapfile
 
 
 umount -l /mnt
 
-mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /dev/nvme0n1p3 /mnt
+mkdir -p /mnt/{boot/efi,home,swapfile,.snapshots,var/{cache,log,lib/libvirt/images}}
 
-mkdir -p /mnt/{boot/efi,home,.snapshots,var/{cache,log,lib/libvirt/images}}
-
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@home /dev/nvme0n1p3 /mnt/home
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@cache /dev/nvme0n1p3 /mnt/var/cache
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@log /dev/nvme0n1p3 /mnt/var/log
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@log /dev/nvme0n1p3 /mnt/var/lib/libvirt/images
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@.snapshots /dev/nvme0n1p3 /mnt/.snapshots
+mount -o defaults,discard=async,ssd,subvol=@home /dev/nvme0n1p2 /mnt/home
+mount -o defaults,discard=async,ssd,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
+mount -o defaults,discard=async,ssd,subvol=@log /dev/nvme0n1p2 /mnt/var/log
+mount -o defaults,discard=async,ssd,subvol=@log /dev/nvme0n1p2 /mnt/var/lib/libvirt/images
+mount -o defaults,discard=async,ssd,subvol=@.snapshots /dev/nvme0n1p2 /mnt/.snapshots
+mount -o defaults,discard=async,ssd,subvol=@swapfile /dev/nvme0n1p2 /mnt/swapfile
 
 mount /dev/nvme0n1p1 /mnt/boot/efi
 
