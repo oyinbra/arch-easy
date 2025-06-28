1ca5fd39f94ff87e8dd58ead8dbd3524d0e4a5b3
Onyinbrakeme  Kelvin Ogbe <35059691+oyinbra@users.noreply.github.com>
2025-06-28 15:51:19 +0000

    Create yay-installer.sh

---
+++ yay-installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Set AUR directory
+AUR_DIR="$HOME/aur"
+YAY_DIR="$AUR_DIR/yay"
+
+# Prepare directory
+mkdir -p "$AUR_DIR"
+cd "$AUR_DIR"
+
+# Clone yay if not already present
+if [ ! -d "$YAY_DIR" ]; then
+    git clone https://aur.archlinux.org/yay.git
+else
+    echo "yay already cloned at $YAY_DIR"
+fi
+
+# Build and install
+cd "$YAY_DIR"
+makepkg -si
