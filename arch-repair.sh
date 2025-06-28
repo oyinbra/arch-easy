d4754f0d905aa52749c7e5497b34bf11e9e9d5c4
Onyinbrakeme  Kelvin Ogbe <35059691+oyinbra@users.noreply.github.com>
2025-06-28 13:58:21 +0000

    Update arch-repair.sh
    update to /boot/efi

--- arch-repair.sh
+++ arch-repair.sh
@@ -2,14 +2,32 @@
 set -e
 
 # -------------------------------------
+# Define partitions
+# -------------------------------------
+EFI=/dev/nvme0n1p1
+ROOT=/dev/nvme0n1p3
+
+# -------------------------------------
 # Mount all BTRFS subvolumes for chroot
 # -------------------------------------
-mount -o noatime,compress=zstd,subvol=@ /dev/nvme0n1p3 /mnt
-mount -o noatime,compress=zstd,subvol=@home /dev/nvme0n1p3 /mnt/home
-mount -o noatime,compress=zstd,subvol=@log /dev/nvme0n1p3 /mnt/var/log
-mount -o noatime,compress=zstd,subvol=@cache /dev/nvme0n1p3 /mnt/var/cache
-mount -o noatime,compress=zstd,subvol=@snapshots /dev/nvme0n1p3 /mnt/.snapshots
-mount /dev/nvme0n1p1 /mnt/boot
+echo "[*] Mounting root subvolumes..."
 
+mount -o noatime,compress=zstd,subvol=@ "$ROOT" /mnt
+mkdir -p /mnt/{boot/efi,home,var/log,var/cache,.snapshots}
+
+mount -o noatime,compress=zstd,subvol=@home       "$ROOT" /mnt/home
+mount -o noatime,compress=zstd,subvol=@log        "$ROOT" /mnt/var/log
+mount -o noatime,compress=zstd,subvol=@cache      "$ROOT" /mnt/var/cache
+mount -o noatime,compress=zstd,subvol=@snapshots  "$ROOT" /mnt/.snapshots
+
+# -------------------------------------
+# Mount EFI
+# -------------------------------------
+echo "[*] Mounting EFI partition..."
+mount "$EFI" /mnt/boot/efi
+
+# -------------------------------------
+# Enter chroot
+# -------------------------------------
 echo "[+] All subvolumes mounted. Entering chroot..."
 arch-chroot /mnt
